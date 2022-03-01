56e83b69f3adca6ea09393a16840c0f4e4fdf8c3
FIRAT <benfiratkaya@gmail.com>
2022-03-01 21:18:41 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp");
+const { series } = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const del = require("del");
+
+// Styles
+function styles() {
+  return gulp
+    .src("src/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist"));
+}
+
+function minify() {
+  return gulp
+    .src("dist/*.css")
+    .pipe(
+      cleanCSS({ debug: true }, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      })
+    )
+    .pipe(rename({ extname: ".min.css" }))
+    .pipe(gulp.dest("dist"));
+}
+
+function clean() {
+  return del("dist/**", { force: true });
+}
+
+function watch(event) {
+  console.log(
+    "File " + event.path + " was " + event.type + ", running tasks..."
+  );
+  styles();
+}
+
+exports.clean = clean;
+exports.styles = styles;
+exports.watch = watch;
+exports.build = series(clean, styles, minify);
+exports.default = series(clean, styles);
